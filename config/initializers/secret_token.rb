29ad765fe05124bd6802015acc4275d79640f84d
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-06-09 18:13:28 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nfs2012Web::Application.config.secret_token = 'cb19a0a4e4832b899f56624171bb452c5af9054cc21596eeafaa400079e184105f9680685f4d1f01c45806da235e1c0739acfcf4971e22d0c3882fac430aea12'
